151c114b1ae5fb1381ba4955f029cdfc038257b0
Alexandra Vakulenko <91200669+AlexaV2002@users.noreply.github.com>
2022-10-06 01:21:41 +0300

    Функция вычитания неисправна

--- MatCalc.cpp
+++ MatCalc.cpp
@@ -1,7 +1,4 @@
-﻿// MatCalc.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 int** CreateMatrix(int n)
@@ -16,7 +13,7 @@ int** CreateMatrix(int n)
 
 void FillMatrix(int** m, int n, int s)
 {
-    cout << "Заполните матрицу " <<s<<":\n";
+    cout << "Заполните матрицу " << s << ":\n";
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             cin >> m[i][j];
@@ -30,11 +27,10 @@ void ShowMatrix(int** m, int n)
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
-            cout << m[i][j]<<" ";
+            cout << m[i][j] << " ";
         cout << "\n";
     }
 }
-
 
 
 
@@ -43,6 +39,51 @@ void DelMatrix(int** m, int n)
     for (int i = 0; i < n; i++)
         delete[] m[i];
     delete[] m;
+}
+
+
+void SubsMatrix(int** A, int** B, int** buff, int n)
+{
+
+}
+
+
+
+void TestSub()
+{
+    int n = 3;
+    int** M1 = CreateMatrix(n);
+    int** M2 = CreateMatrix(n);
+    int** b = CreateMatrix(n);
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            M1[i][j] = i + j;
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            M2[i][j] = i + j;
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            b[i][j] = -1;
+
+    SubsMatrix(&(*M1), &(*M2), &(*b), n);
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            if (b[i][j] != 0)
+            {
+                cout << "\nОшибка, неверно вычтена матрица\n";
+                DelMatrix(&(*M1), n);
+                DelMatrix(&(*M2), n);
+                DelMatrix(&(*b), n);
+                return;
+            }
+    cout << "\nФункция сработала успешно\n";
+    DelMatrix(&(*M1), n);
+    DelMatrix(&(*M2), n);
+    DelMatrix(&(*b), n);
 }
 
 
@@ -59,6 +100,16 @@ int main()
     int** m2 = CreateMatrix(N);
     FillMatrix(&(*m2), N, 2);
     ShowMatrix(&(*m2), N);
+
+    int** mbuff = CreateMatrix(N);
+    cout << "\nРазность матриц 1 и 2\n";
+    SubsMatrix(&(*m1), &(*m2), &(*mbuff), N);
+    ShowMatrix(&(*mbuff), N);
+
+    TestSub();
+
+    DelMatrix(&(*m1), N);
+    DelMatrix(&(*m2), N);
 
     return 0;
 }
